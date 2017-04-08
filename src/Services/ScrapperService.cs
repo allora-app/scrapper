6fe40ea9ceea2d1522dc5d05157038dc245a6ab1
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2017-04-08 15:10:24 +0300

    feat(scrapper): save parsed verb through firebase service

--- src/Services/ScrapperService.cs
+++ src/Services/ScrapperService.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Blinnikov.Scrapper.Models;
+using Blinnikov.Scrapper.Store;
 using Newtonsoft.Json;
 
 namespace Blinnikov.Scrapper.Services 
@@ -11,20 +13,23 @@ namespace Blinnikov.Scrapper.Services
         private readonly IRecordsLoader _recordsLoader;
         private readonly IRecordNormalizer _normalizer;
         private readonly IVerbBuilder _verbBuilder;
+        private readonly IFirebaseClient _firebaseClient;
 
         public ScrapperService()
         {
             this._recordsLoader = new RecordsLoader();
             this._normalizer = new RecordNormalizer();
             this._verbBuilder = new VerbBuilder();
+            this._firebaseClient = new FirebaseClient("allora-conjugator");
         }
 
         public async Task<string> Run()
         {
-            var fare = 3832;
+            // var fare = 3832;
             var vedere = 12107;
             var records = await this._recordsLoader.Load(vedere);
             var verb = this._verbBuilder.Build(records);
+            var savedVerb = await this._firebaseClient.Save<Verb>(verb);
 
             var json = JsonConvert.SerializeObject(verb, Formatting.Indented);
             Console.WriteLine("Built Verb:");
